d46ff099d925a0391ad806ac9c6bff2388df4832
anguita <alvaro-miguel.anguita-palomino@capgemini.com>
2021-11-25 09:23:05 +0100

    Daos creados

---
+++ src/main/java/com/tarea2/dao/GrupoDao.java
@@ -0,0 +1,9 @@
+package com.tarea2.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface GrupoDao extends CrudRepository<Grupo, Long> {
+
+}
---
+++ src/main/java/com/tarea2/dao/ProyectoDao.java
@@ -0,0 +1,9 @@
+package com.tarea2.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public  interface ProyectoDao extends CrudRepository<Proyecto, Long> {
+
+}
