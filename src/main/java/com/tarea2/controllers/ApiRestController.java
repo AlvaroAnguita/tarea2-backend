5971c5776ee83fbaf4bdcf4f3a118c6742458c34
Erik <erikdp9857@gmail.com>
2021-11-25 13:27:38 +0100

    Primeros Metodos clase Proyecto

--- src/main/java/com/tarea2/controllers/ApiRestController.java
+++ src/main/java/com/tarea2/controllers/ApiRestController.java
@@ -6,13 +6,20 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tarea2.entity.Proyecto;
 import com.tarea2.entity.Trabajador;
 import com.tarea2.services.GrupoService;
 import com.tarea2.services.ProyectoService;
@@ -57,6 +64,59 @@ public class ApiRestController {
 		return new ResponseEntity<Trabajador>(t,HttpStatus.OK);
 	}
 	
+	//CRUD
+	//POST Project
+	@PostMapping("/proyectos")
+	public ResponseEntity<?> createProject(@RequestBody Proyecto proyecto) {
+		Map<String, Object> response = new HashMap<>();
+			try {
+				proyectoService.save(proyecto);
+
+			} catch (DataAccessException e) {
+				response.put("Mensaje", "No se puede crear el proyecto");
+				response.put("Error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			response.put("Existoso", "Se ha creado el proyecto correctamente");
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+	}	
+	
+	
+	//SHOW Projects
+	@GetMapping
+	public ResponseEntity<?> showProject(@RequestParam Proyecto proyecto){
+		Map<String, Object> response = new HashMap<String, Object>();
+		try {
+			proyectoService.findAll();
+		} catch (DataAccessException e) {
+			response.put("Error", "No se puede mostrar los proyectos");
+			response.put("Incorrecto", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String,Object>> (response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("Existoso", "Se muestran los proyectos");
+		return new ResponseEntity<Map<String, Object>> (response, HttpStatus.OK);
+	}
+	
+	
+	public ResponseEntity<?> showProjectById(@PathVariable Long id){
+		Map<String,Object> response = new HashMap<String, Object>();
+		try {
+			proyectoService.findById(id);
+		} catch (DataAccessException e) {
+			response.put("Error", "No se puede mostrar el proyecto con id: "+id);
+			response.put("Incorrecto", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>> (response, HttpStatus.OK);
+	
+		}
+		response.put("Exitoso", "Se muestra proyecto con id: "+id);
+		return new ResponseEntity<Map<String,Object>> (response, HttpStatus.OK);
+		
+		
+	}
+	
+	
+}
+	
 	
 
-}
+
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO trabajadores (dni,pass,nombre,apellido,email,telefono,direccion,departamento,cargo,activo,admin) VALUES(00000000A,'admin','Admin',null,'admin@hotmail.com',65433223,'Calle Capgemini',null ,null ,null,true);
+INSERT INTO trabajadores (dni,pass,nombre,apellido,email,telefono,direccion,departamento,cargo,activo,admin) VALUES(56297846S,'12345','Juan','Redondo','jredo@hotmail.com',698730153,'Calle Torresuso','I+D' ,'Desarrollador' ,null,false);
